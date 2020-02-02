a86742294e57b0a32fa2832c500671bc611023ab
Soumyadip Pal <soumyadip.pal@infosys.com>
2020-02-03 04:25:51 +0530

    Updates

---
+++ precache-manifest.b8419720689be5454cacd1f295de000d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a138052b1e8baf9e1ff1adf0f03020",
+    "url": "/chat-box/index.html"
+  },
+  {
+    "revision": "7eb9b9bbba286614bb65",
+    "url": "/chat-box/static/css/main.7c9bf0f1.chunk.css"
+  },
+  {
+    "revision": "7e0f04cf6ccbe7c41a5f",
+    "url": "/chat-box/static/js/2.9aa0cfba.chunk.js"
+  },
+  {
+    "revision": "8345da3dc262b15c21e065faca7dd545",
+    "url": "/chat-box/static/js/2.9aa0cfba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb9b9bbba286614bb65",
+    "url": "/chat-box/static/js/main.6beaf441.chunk.js"
+  },
+  {
+    "revision": "b996ebce41b87a68d3a5",
+    "url": "/chat-box/static/js/runtime-main.8d256574.js"
+  }
+]);
